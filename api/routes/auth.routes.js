bf950bc00a3296c8c70682050a744dfd3d1da48a
sadaf <sadaf@Sadafs-MacBook-Air.local>
2025-12-16 15:05:23 +0400

    Stop tracking node_modules and ignore env file

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -6,6 +6,9 @@ import {
   enable2FA,
   verify2FA,
   verifyLogin2FA,
+  loginWithEmail,
+  verifyEmailCode,
+  sendMagicLink,
 } from "../controllers/auth.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
@@ -13,9 +16,15 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.post("/logout", logout);
+
+router.post("/login-email", loginWithEmail);
+router.post("/verify-email-code", verifyEmailCode);
+router.post("/magic-link", sendMagicLink);
+
 router.post("/enable-2fa", verifyToken, enable2FA);
 router.post("/verify-2fa", verifyToken, verify2FA);
 router.post("/verify-login-2fa", verifyLogin2FA);
+
+router.post("/logout", logout);
 
 export default router;
